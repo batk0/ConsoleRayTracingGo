2a3d026fb019351b62d4bd89f3d5ddf2139ff621
Anton Kaiukov <batkowm@gmail.com>
2022-07-31 20:55:56 -0700

    Go routines and common interface for objects

--- main.go
+++ main.go
@@ -5,16 +5,37 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
 	"time"
 
 	"golang.org/x/sys/unix"
 )
 
+const gradient = " .:!/r(l1Z4H9W8$@"
+const gradientSize = len(gradient) - 1
+
 func main() {
+	// go routines setup
+	var workers uint16 = 16
+	done := make(chan struct{})
+	defer close(done)
+	fOut := make([]<-chan row, workers)
+
+	// Signals handling
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	hideCursor()
+	defer showCursor()
+	go func() {
+		<-sigs
+		close(done)
+		showCursor()
+		fmt.Println("Exiting")
+		os.Exit(0)
+	}()
 
+	// Screen setup
 	var width uint16 = 120
 	var height uint16 = 30
 	if ws, err := unix.IoctlGetWinsize(syscall.Stdout, unix.TIOCGWINSZ); err == nil {
@@ -24,66 +45,41 @@ func main() {
 	}
 	aspect := float64(width) / float64(height)
 	pixelAspect := 11.0 / 24
-	gradient := " .:!/r(l1Z4H9W8$@"
-	gradientSize := len(gradient) - 1
 
-	screen := newScreen(height, width)
+	// Setup common params for rows
+	light := vec.Vec3FromXYZ(-0.5, 0.5, -1.0).Norm()
+	objects := []vec.Object{
+		vec.Box{Size: vec.Vec3FromScalar(1), Position: vec.Vec3FromXYZ(0, 3, 0)},
+		// vec.Sphere{Radius: 1, Position: vec.Vec3FromXYZ(0, 3, 0)},
+		vec.Sphere{Radius: 1, Position: vec.Vec3FromXYZ(3, 0, 0)},
+		vec.Sphere{Radius: 1, Position: vec.Vec3FromXYZ(0, -3, 0)},
+		vec.Sphere{Radius: 1, Position: vec.Vec3FromXYZ(-3, 0, 0)},
+		vec.Box{Size: vec.Vec3FromScalar(1), Position: vec.Vec3FromXYZ(0, 0, -1)},
+		vec.Plane{Normal: vec.Vec3FromXYZ(0, 0, 1), Position: vec.Vec3FromXYZ(0, 0, 2)},
+	}
+	params := rowParams{width, height, 0, aspect, pixelAspect, 0, objects, light}
+
+	// Start timer
 	ts_start := float64(time.Now().UnixMilli())
 	ts := ts_start
 	tsOld := ts
-	hideCursor()
-	defer showCursor()
-	go func() {
-		<-sigs
-		showCursor()
-		fmt.Println("Exiting")
-		os.Exit(0)
-	}()
+
 	for {
 		// Main loop
 		t := float64(time.Now().UnixMilli()) - ts_start
-		light := vec.Vec3FromXYZ(-0.5, 0.5, -1.0).Norm()
-		spheres := []vec.Vec3{
-			vec.Vec3FromXYZ(0, 3, 0),
-			vec.Vec3FromXYZ(3, 0, 0),
-			vec.Vec3FromXYZ(0, -3, 0),
-			vec.Vec3FromXYZ(-3, 0, 0),
+		params.t = t
+
+		// Start workers
+		for w := uint16(0); w < workers; w++ {
+			// fmt.Println(w)
+			fOut[w] = fanOut(done, genRowParams(done, params, height, workers, w))
 		}
-		for j := 0; j < int(height); j++ {
-			for i := 0; i < int(width); i++ {
-				uv := vec.Vec2FromXY(float64(i), float64(j)).Div(vec.Vec2FromXY(float64(width), float64(height))).Mul(vec.Vec2FromScalar(2)).Sub(vec.Vec2FromScalar(1))
-				uv.X *= aspect * pixelAspect
-				ro := vec.Vec3FromXYZ(-6, 0, 0)
-				rd := vec.Vec3FromVec2(2, uv).Norm()
-				ro = vec.RotateY(ro, 0.25)
-				rd = vec.RotateY(rd, 0.25)
-				ro = vec.RotateZ(ro, float64(t)*0.001)
-				rd = vec.RotateZ(rd, float64(t)*0.001)
-				diff := 1.0
-				for k := 0; k < 5; k++ {
-					minIt := 99999.0
-					n := vec.Vec3FromScalar(0)
-					albedo := 1.0
-					for _, spherePos := range spheres {
-						intersectSphere(ro, spherePos, rd, &minIt, &n)
-					}
-					intersectCube(ro, rd, &minIt, &n)
-					intersectPlane(ro, rd, &minIt, &n, &albedo)
-					if minIt < 99999 {
-						diff *= (n.Dot(light)*0.5 + 0.5) * albedo
-						ro = ro.Add(rd.Mul(vec.Vec3FromScalar(minIt - 0.01)))
-						rd = vec.Reflect(rd, n)
-					} else {
-						break
-					}
-				}
-				color := int(diff * 20)
-				color = int(vec.Clamp(float64(color), 0, float64(gradientSize)))
-				pixel := gradient[color]
-				screen[j].line[i] = pixel
-			}
-			movePrint(0, j, string(screen[j].line))
+		// Print results
+		for r := range fanIn(done, fOut...) {
+			movePrint(0, int(r.n), string(r.line))
 		}
+
+		// Reset timer and print FPS
 		tsOld = ts
 		ts = float64(time.Now().UnixMilli())
 		movePrint(0, int(height)-1, fmt.Sprintf("FPS: %v ", 1000.0/(ts-tsOld)))
@@ -91,30 +87,11 @@ func main() {
 
 }
 
-func intersectPlane(ro vec.Vec3, rd vec.Vec3, minIt *float64, n *vec.Vec3, albedo *float64) {
-	intersection := vec.Vec2FromScalar(vec.Plane(ro, rd, vec.Vec3FromXYZ(0, 0, -1), 1))
-	if intersection.X > 0 && intersection.X < *minIt {
-		*minIt = intersection.X
-		*n = vec.Vec3FromXYZ(0, 0, -1)
-		*albedo = 0.5
-	}
-}
-
-func intersectCube(ro vec.Vec3, rd vec.Vec3, minIt *float64, n *vec.Vec3) {
-	intersection, boxN := vec.Box(ro, rd, vec.Vec3FromScalar(1))
-	if intersection.X > 0 && intersection.X < *minIt {
-		*minIt = intersection.X
-		*n = boxN
-	}
-}
-
-func intersectSphere(ro vec.Vec3, spherePos vec.Vec3, rd vec.Vec3, minIt *float64, n *vec.Vec3) {
-	intersection := vec.Sphere(ro.Sub(spherePos), rd, 1)
-	if intersection.X > 0 && intersection.X < *minIt {
-		itPoint := ro.Sub(spherePos).Add(rd.Mul(vec.Vec3FromScalar(intersection.X)))
-		*minIt = intersection.X
-		*n = itPoint.Norm()
-	}
+type rowParams struct {
+	width, height, j       uint16
+	aspect, pixelAspect, t float64
+	objects                []vec.Object
+	light                  vec.Vec3
 }
 
 type row struct {
@@ -127,12 +104,98 @@ func newRow(width uint16, n uint16) row {
 	return r
 }
 
-func newScreen(height, width uint16) []row {
-	screen := make([]row, height)
-	for i := uint16(0); i < height; i++ {
-		screen[i] = newRow(width, i)
+// Generates rowParams and stream them to the chan
+func genRowParams(done chan struct{}, params rowParams, height, workers, w uint16) <-chan rowParams {
+	r := make(chan rowParams)
+	go func() {
+		defer close(r)
+		for j := w; j < height; j += workers {
+			params.j = j
+			select {
+			case <-done:
+				return
+			case r <- params:
+			}
+		}
+
+	}()
+	return r
+}
+
+func fanOut(done chan struct{}, in <-chan rowParams) <-chan row {
+	resultRow := make(chan row)
+	go func() {
+		defer close(resultRow)
+		for rp := range in {
+			select {
+			case <-done:
+				return
+			case resultRow <- computeRow(rp):
+			}
+		}
+	}()
+	return resultRow
+}
+
+func fanIn(done chan struct{}, cs ...<-chan row) <-chan row {
+	var wg sync.WaitGroup
+	resultRow := make(chan row)
+
+	mx := func(c <-chan row) {
+		defer wg.Done()
+		for r := range c {
+			select {
+			case <-done:
+				return
+			case resultRow <- r:
+			}
+		}
 	}
-	return screen
+	wg.Add(len(cs))
+	for _, c := range cs {
+		go mx(c)
+	}
+
+	go func() {
+		wg.Wait()
+		close(resultRow)
+	}()
+	return resultRow
+}
+
+func computeRow(params rowParams) row {
+	screenRow := newRow(params.width, params.j)
+	for i := 0; i < int(params.width); i++ {
+		uv := vec.Vec2FromXY(float64(i), float64(params.j)).Div(vec.Vec2FromXY(float64(params.width), float64(params.height))).Mul(vec.Vec2FromScalar(2)).Sub(vec.Vec2FromScalar(1))
+		uv.X *= params.aspect * params.pixelAspect
+		ro := vec.Vec3FromXYZ(-10, 0, 0)
+		rd := vec.Vec3FromVec2(2, uv).Norm()
+		ro = vec.RotateY(ro, 0.25)
+		rd = vec.RotateY(rd, 0.25)
+		ro = vec.RotateZ(ro, float64(params.t)*0.001)
+		rd = vec.RotateZ(rd, float64(params.t)*0.001)
+		diff := 1.0
+		for k := 0; k < 5; k++ {
+			minIt := 99999.0
+			n := vec.Vec3FromScalar(0)
+			albedo := 1.0
+			for _, o := range params.objects {
+				o.GetReflection(ro, rd, &minIt, &n, &albedo)
+			}
+			if minIt < 99999 {
+				diff *= (n.Dot(params.light)*0.5 + 0.5) * albedo
+				ro = ro.Add(rd.Mul(vec.Vec3FromScalar(minIt - 0.01)))
+				rd = vec.Reflect(rd, n)
+			} else {
+				break
+			}
+		}
+		color := int(diff * 20)
+		color = int(vec.Clamp(float64(color), 0, float64(gradientSize)))
+		pixel := gradient[color]
+		screenRow.line[i] = pixel
+	}
+	return screenRow
 }
 
 func movePrint(x, y int, str string) {
--- vec/functions.go
+++ vec/functions.go
@@ -40,33 +40,78 @@ func RotateZ(v Vec3, angle float64) Vec3 {
 	return r
 }
 
-func Sphere(ro, rd Vec3, r float64) Vec2 {
+type Object interface {
+	// intersect(ro, rd Vec3) float64
+	GetReflection(ro, rd Vec3, minIt *float64, normal *Vec3, albedo *float64)
+}
+type Sphere struct {
+	Radius   float64
+	Position Vec3
+}
+type Box struct {
+	Size     Vec3
+	Position Vec3
+}
+type Plane struct {
+	Normal   Vec3
+	Position Vec3
+}
+
+func (s Sphere) intersect(ro, rd Vec3) float64 {
+	// Solving quadratic equation where a=1
 	b := ro.Dot(rd)
-	c := ro.Dot(ro) - r*r
+	c := ro.Dot(ro) - s.Radius*s.Radius
 	h := b*b - c
 	if h < 0 {
-		return Vec2FromScalar(-1)
+		return -1
 	}
 	h = math.Sqrt(h)
-	return Vec2FromXY(-h-b, h-b)
+	return -h - b // We don't use second root h-b
 }
-func Box(ro, rd, boxSize Vec3) (Vec2, Vec3) {
+
+func (b Box) intersect(ro, rd Vec3) (float64, Vec3) {
 	m := Vec3FromScalar(1).Div(rd)
 	n := m.Mul(ro)
-	k := m.Abs().Mul(boxSize)
+	k := m.Abs().Mul(b.Size)
 	t1 := n.Neg().Sub(k)
 	t2 := n.Neg().Add(k)
-	tN := math.Max(math.Max(t1.X, t1.Y), t1.Z)
-	tF := math.Min(math.Min(t2.X, t2.Y), t2.Z)
+	tN := math.Max(math.Max(t1.X, t1.Y), t1.Z) // near point of intersection
+	tF := math.Min(math.Min(t2.X, t2.Y), t2.Z) // far point of intersection
 	if tN > tF || tF < 0 {
-		return Vec2FromScalar(-1), Vec3FromScalar(0)
+		return -1, Vec3FromScalar(0)
 	}
 	yzx := Vec3FromXYZ(t1.Y, t1.Z, t1.X)
 	zxy := Vec3FromXYZ(t1.Z, t1.X, t1.Y)
 	outNormal := rd.Sign().Neg().Mul(yzx.Step(t1)).Mul(zxy.Step(t1))
-	return Vec2FromXY(tN, tF), outNormal
+	return tN, outNormal // We don't use tF
 }
 
-func Plane(ro, rd, p Vec3, w float64) float64 {
-	return -(ro.Dot(p) + w) / rd.Dot(p)
+func (p Plane) intersect(ro, rd Vec3) float64 {
+	return ro.Neg().Dot(p.Normal) / rd.Dot(p.Normal)
+}
+
+func (p Plane) GetReflection(ro Vec3, rd Vec3, minIt *float64, normal *Vec3, albedo *float64) {
+	intersection := p.intersect(ro.Sub(p.Position), rd)
+	if intersection > 0 && intersection < *minIt {
+		*minIt = intersection
+		*normal = Vec3FromXYZ(0, 0, -1)
+		*albedo = 0.5
+	}
+}
+
+func (b Box) GetReflection(ro Vec3, rd Vec3, minIt *float64, normal *Vec3, albedo *float64) {
+	intersection, boxN := b.intersect(ro.Sub(b.Position), rd)
+	if intersection > 0 && intersection < *minIt {
+		*minIt = intersection
+		*normal = boxN
+	}
+}
+
+func (s Sphere) GetReflection(ro Vec3, rd Vec3, minIt *float64, normal *Vec3, albedo *float64) {
+	intersection := s.intersect(ro.Sub(s.Position), rd)
+	if intersection > 0 && intersection < *minIt {
+		itPoint := ro.Sub(s.Position).Add(rd.Mul(Vec3FromScalar(intersection)))
+		*minIt = intersection
+		*normal = itPoint.Norm()
+	}
 }
